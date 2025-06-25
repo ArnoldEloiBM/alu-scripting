7e6e91a5257ebc89bda13418548cd789aef811ae
ArnoldEloiBM <a.buyangem@alustudent.com>
2025-06-25 09:18:27 -0700

    task0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
+"""Return the number of subscribers of a given subreddit"""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'MyBot/0.0.1'}
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
 
-    # Construct the URL for the subreddit
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
 
-    # Make the request, allowing redirects but checking the final URL
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the subreddit is valid by ensuring no redirect and status code 200
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data', {}).get('subscribers', 0)
-    else:
+    except Exception:
         return 0
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
