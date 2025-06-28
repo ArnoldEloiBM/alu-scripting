17344b1d0cddc7e733ad1a725da77ec127583efe
ArnoldEloiBM <a.buyangem@alustudent.com>
2025-06-28 16:20:21 +0200

    new changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""
+Function that queries Reddit API and prints titles of first 10 hot posts for a given subreddit.
+If subreddit is invalid, prints None.
+"""
+
 import requests
 
+
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a given subreddit."""
+    """Prints titles of first 10 hot posts for given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'MyBot/0.0.1'}  # Reddit requires a custom User-Agent
-    
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
