fb4a0858a68abbbb00f6f0f5c483587ad43f4fcb
ArnoldEloiBM <a.buyangem@alustudent.com>
2025-07-05 12:12:28 +0200

    new changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""
+Function that queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+"""
 import requests
 
+
 def top_ten(subreddit):
-    """ Prints the titles of the first 10 hot posts listed for a given subreddit. """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'Chrome/1.0'}
-
-    # Make the request and prevent redirects
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the subreddit is valid
-    if response.status_code != 200:
-        print(None)
-        return
-
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+    If the subreddit is invalid, prints None.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'python:subreddit.hot.posts:v1.0 (by /u/yourusername)'}
+    
     try:
-        data = response.json()
-    except ValueError:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except requests.RequestException:
         print(None)
-        return
-
-    # Extract and print the titles of the first 10 hot posts
-    posts = data.get('data', {}).get('children', [])
-    for post in posts:
-        print(post.get('data', {}).get('title'))
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
