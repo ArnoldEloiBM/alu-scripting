18c6544e9731ef29f4045254f88c13396b48128b
ArnoldEloiBM <a.buyangem@alustudent.com>
2025-07-05 13:55:22 +0200

    top10

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Chrome/1.0'}
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     # print(response)
     print(response.text[189097:])
