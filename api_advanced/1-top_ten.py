a41646706fb6e11096173499e6b6732a73965665
ArnoldEloiBM <a.buyangem@alustudent.com>
2025-07-05 14:12:36 +0200

    advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+""" top_ten.py """
 import requests
-import sys
 
 
 def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
-
-    if response.status_code == 200:
-        json_data = response.json()
-        posts = json_data.get('data', {}).get('children', [])
-        if posts:
-            for i in range(min(10, len(posts))):
-                print(posts[i].get('data', {}).get('title'))
-        else:
-            sys.stdout.write("OK")
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    # print(response)
+    print(response.text[189097:])
+    if response.status_code != 200:
+        print(None)
     else:
-        sys.stdout.write("OK")
+        posts = response.json().get('data').get('children')
+        for post in posts:
+            print(post['data']['title'])
