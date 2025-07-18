9309f58736aa400735bc7fa1ce22976fa1635fe0
ArnoldEloiBM <a.buyangem@alustudent.com>
2025-07-18 00:37:35 -0700

    new changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,12 +7,11 @@ def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(subreddit_url, headers=headers)
+
     # print(response)
-    print(response.text[189097:])
-    if response.status_code != 200:
-        print(None)
-    else:
-        posts = response.json().get('data').get('children')
-        for post in posts:
-            print(post['data']['title'])
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(json_data.get('data', {}).get('children', [None]*10)[i].get('data',{}).get('title'))
+        else: print(None)
